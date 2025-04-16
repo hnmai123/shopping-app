979f17d268f49cd8c7373e846c8e23a0095b265c
Huy Nhat Mai <nhathuylk0123@gmail.com>
2025-04-16 22:42:02 +1000

    Add firebase firestore connection and basic homepage layout

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,134 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, View, Text, SafeAreaView, TextInput, Image, ActivityIndicator} from 'react-native';
+import { useEffect, useState } from 'react';
+import { doc, getDocs, collection } from 'firebase/firestore';
+import { db } from '../../firebase/firebaseConfig';
+import { FlatList, GestureHandlerRootView } from 'react-native-gesture-handler';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<{ id: string; category: string; description: string; image: string; name: string; price: number }[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const fetchData = async () => {
+      try {
+        const colref = collection(db, 'products');
+        const snapshot = await getDocs(colref);
+        const fetchedData = snapshot.docs.map((doc) => {
+          const docData = doc.data();
+          return {
+            id: doc.id,
+            category: docData.category,
+            description: docData.description,
+            image: docData.image,
+            name: docData.name,
+            price: docData.price,
+          };
+        });
+        setData(fetchedData);
+        setIsLoading(false);
+        console.log(fetchedData);
+      } catch (err) {
+        console.error('Error fetching data:', err);
+        setIsLoading(false);
+        setError('Failed to fetch data');
+      }
+    };
+
+    fetchData();
+  }, []);
+
+
+  const handleSearch = (query: string) => {
+    setSearchQuery(query);
+    console.log(query);
+  }
+
+  if (isLoading) {
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#61EDFF' }}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      </GestureHandlerRootView>
+    );
+  }
+
+  if (error) {
+    return (
+      <GestureHandlerRootView style={{ flex: 1 }}>
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#61EDFF' }}>
+          <Text>{error}</Text>
+        </View>
+      </GestureHandlerRootView>
+    );
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <TextInput
+          placeholder="Search ..."
+          clearButtonMode="always"
+          style={styles.searchBox}
+          autoCapitalize="none"
+          autoCorrect={false}
+          value={searchQuery}
+          onChangeText={(query) => handleSearch(query)}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        <FlatList
+          data={data}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <View style = {styles.productCard}>
+              <Image source={{ uri: item.image }} style={styles.productImage} />
+              <View>
+                <Text>{item.name}</Text>
+              </View>
+            </View>
+          )}
+          numColumns={2}
+          contentContainerStyle={styles.productList}
+        />
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
+    backgroundColor: '#61EDFF',
+    paddingTop: 20,
+  },
+  searchBox: {
+    width: '90%',
+    height: 46,
+    borderRadius: 10,
+    backgroundColor: '#FFFFFF',
+    paddingHorizontal: 10,
+    marginLeft: '2%',
+    marginBottom: 20,
+  },
+  productCard: {
+    flexDirection: 'column',
     alignItems: 'center',
-    gap: 8,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 10,
+    padding: 5,
+    marginVertical: "2%",
+    marginHorizontal: "2%",
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  productImage: {
+    width: 125,
+    height: 125,
+    borderRadius: 5,
+    marginRight: 10,
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  productList: {
+
+  }
 });
