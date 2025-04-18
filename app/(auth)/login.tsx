fb8ce29c1b322ed26898385ce5da2081cf18cdf8
Huy Nhat Mai <nhathuylk0123@gmail.com>
2025-04-18 18:24:26 +1000

    Update login function and navigation

--- app/(auth)/login.tsx
+++ app/(auth)/login.tsx
@@ -1,16 +1,72 @@
-import { SafeAreaView, StyleSheet, View, Text, Image} from 'react-native';
+import { SafeAreaView, StyleSheet, View, Text, Image, TextInput, TouchableOpacity} from 'react-native';
 import { useRouter } from 'expo-router';
+import { useState } from 'react';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+import { auth } from '../../firebase/firebaseConfig';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
 export default function LoginScreen() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("Login successful");
+      router.replace('/(tabs)');
+    }
+    catch (err: any) {
+      console.error("Login error: ", err.message);
+    }
+  };
+
   return (
     <SafeAreaView style = {styles.containter}>
-      <View style={styles.header}>
-        <Image source={require('../../assets/images/favicon.png')} style={styles.logo} />
-        <Text style = {styles.appName}>PACKME</Text>
-        <Text style = {styles.appTarget}>All in one click</Text>
-      </View>
-      <View style={styles.loginContainer}>
-        <Text style={styles.welcomeBack}>Welcome back</Text>
+      <View style={styles.mainContainer}>
+        <View style={styles.header}>
+          <Image source={require('../../assets/images/favicon.png')} style={styles.logo} />
+          <Text style = {styles.appName}>PACKME</Text>
+          <Text style = {styles.appTarget}>All in one click</Text>
+        </View>
+        <View style={styles.loginContainer}>
+          <Text style={styles.welcomeBack}>Welcome back</Text>
+          <TextInput 
+            placeholder="Enter your email or username" 
+            style={styles.loginField} 
+            placeholderTextColor={"#999999"}
+            value={email}
+            autoCapitalize='none'
+            autoCorrect={false}
+            onChangeText={setEmail}
+          />
+          <View style={styles.passwordContainer}>
+            <TextInput 
+              placeholder="Enter your password"  
+              placeholderTextColor={"#999999"}
+              secureTextEntry={!showPassword}
+              value={password}
+              autoCapitalize='none'
+              autoCorrect={false}
+              onChangeText={setPassword}
+              style={{flex: 0.95, fontSize: 15}}
+            />
+            <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+              <MaterialIcons name={!showPassword ? 'visibility-off' : 'visibility'} size={25} color="#666666" />
+            </TouchableOpacity>
+          </View>
+          <TouchableOpacity style={styles.loginButton} onPress={handleLogin}>
+            <Text style={styles.loginButtonText}>Log in</Text>
+          </TouchableOpacity>
+          <View style={styles.optionRows}>
+            <Text style={styles.optionText}>Log in via SMS</Text>
+            <Text style={styles.optionText}>Forgot password?</Text>
+          </View>
+          <Text style={styles.signUpText}>
+            New to Packme? <Text style={styles.signUpLink}>Sign up</Text>
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -19,10 +75,14 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
   containter: {
     flex: 1,
-    backgroundColor: '#61EDFF',
+    backgroundColor: '#e9f5f9',
+  },
+  mainContainer: {
+    flex: 1,
   },
   header: {
     backgroundColor: '#61EDFF',
+    padding: "6%",
   },
   appName: {
     fontSize: 50,
@@ -35,7 +95,7 @@ const styles = StyleSheet.create({
     fontSize: 35,
     textAlign: 'center',
     color: 'black',
-    marginBottom: 125
+    marginBottom: 100
   },
   logo: {
     width: 131,
@@ -55,5 +115,61 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 20,
     marginBottom: 20
+  },
+  loginField: {
+    backgroundColor: '#86eff5',
+    borderRadius: 10,
+    width: '78%',
+    marginBottom: 10,
+    height: 45,
+    alignSelf: 'center',
+    paddingLeft: 10,
+    fontSize: 15
+  },
+  passwordContainer: {
+    width: '78%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#86eff5',
+    height: 45,
+    borderRadius: 10,
+    alignSelf: 'center',
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+  loginButton: {
+    width: '78%',
+    backgroundColor: '#00B1BA',
+    height: 45,
+    borderRadius: 10,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  },
+  loginButtonText: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  optionRows: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '78%',
+    alignSelf: 'center',
+  },
+  optionText: {
+    fontSize: 10,
+    color: '#808080',
+    padding: 5,
+    marginBottom: 5
+  },
+  signUpText: {
+    fontSize: 12,
+    color: '#808080',
+    textAlign: 'center',
+  },
+  signUpLink: {
+    fontSize: 12,
+    color: '#00B1BA',
   }
 });
