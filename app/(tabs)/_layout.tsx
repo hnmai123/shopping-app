654028978ac47d23f7140f6e04fc53cec5613ccf
Huy Nhat Mai <nhathuylk0123@gmail.com>
2025-05-12 21:26:15 +1000

    fix the navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -32,13 +32,7 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <Ionicons size={28} name="home" color={color} />,
         }}
       />
-      <Tabs.Screen
-        name="notifications"
-        options={{
-          title: 'Notifications',
-          tabBarIcon: ({ color }) => <Ionicons size={28} name="notifications-outline" color={color} />,
-        }}
-      />
+      
       <Tabs.Screen
         name="(cart)"
         options={{
@@ -51,6 +45,13 @@ export default function TabLayout() {
         options={{
           title: 'Orders',
           tabBarIcon: ({ color }) => <Ionicons size={28} name="bag-check-outline" color={color} />,
+        }}
+      />
+      <Tabs.Screen
+        name="notifications"
+        options={{
+          title: 'Notifications',
+          tabBarIcon: ({ color }) => <Ionicons size={28} name="notifications-outline" color={color} />,
         }}
       />
       <Tabs.Screen
